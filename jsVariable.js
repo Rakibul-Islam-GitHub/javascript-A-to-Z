6aba2754911bb89c9e2a1c2dbed03a04bf2eccff
Rakibul-Islam-GitHub <freelancing.everyday@gmail.com>
2021-08-15 14:28:23 +0600

    javascript variables

---
+++ jsVariable.js
@@ -0,0 +1,24 @@
+console.log(7);
+
+var name= 'Rakibul';  //string
+var price = 10.5;   //float
+var mark = 96;      //integer
+var myBoolean = true;       // a boolean
+
+// printing of these variables 
+console.log(name);
+console.log(mark);
+console.log(price);
+console.log(myBoolean);
+
+
+
+/*  However, the null value is a different type of value, 
+and is used when a variable should be marked as empty. 
+undefined can be used for this purpose, 
+but it should not be used.   */
+
+var emptyVariable = null;
+console.log(emptyVariable);
+
+
