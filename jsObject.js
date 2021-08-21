6d0627c1f9e1b7ba5fcade97968cc24ce132afee
Rakibul-Islam-GitHub <freelancing.everyday@gmail.com>
2021-08-21 12:08:34 +0600

    javascript object example

---
+++ jsObject.js
@@ -0,0 +1,20 @@
+//************ */ javascript object start ***************
+student = {
+    name: 'Rakibul', 
+    id:111,
+    dep: 'CSE'
+}
+console.log(student.name);
+
+/* to change a value  */
+student.id= 452;
+student['dep']= 'EEE';  // other way
+console.log(student.id);
+console.log(student.dep);
+
+/* adding new property */
+student.age= 23;
+console.log(student.age);
+console.log(student);
+//************* */ javascript object start end *************
+
