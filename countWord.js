6830af9f2b1437136c0130ade2b7396db0c2084e
Rakibul-Islam-GitHub <freelancing.everyday@gmail.com>
2021-09-11 23:07:39 +0600

    word count

---
+++ countWord.js
@@ -0,0 +1,13 @@
+function getWordCount(str) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === ' ' || str[i]===str[str.length-1]) {
+        count++;
+      }
+    }
+    return count;
+  }
+  let str = 'Rakibul Islam';
+  let str2= "I will count the word from this string";
+  console.log(getWordCount(str));  // 2
+  console.log(getWordCount(str2));  // 8
